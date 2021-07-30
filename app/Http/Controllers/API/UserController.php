1219c438b63c0dcffb77d0db120f1d580ced0b9f
Aliff <aliff.rosli96@gmail.com>
2021-07-30 09:16:39 +0800

    User: function adjusted: crud form to api dev

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,16 +9,37 @@ use Illuminate\Support\Facades\Http;
 
 class UserController extends Controller
 {
+    // public function index(Request $request)
+    // {
+    //     return view('welcome',compact('users'));
+    // }
+
     public function index(Request $request)
     {
-    return view('index');
+        $url = 'http://api-training.test/api/v1/users';
+        
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYzVlNDg0MjhlMmRmNTYwNjVjNjBjODNmOGM3Y2JhN2I2MGJlODExYWMwZmJkNzQyNjZiNDI0Zjc4M2IyZjgzODVhNWIyMjE4NzM5Zjg4MDciLCJpYXQiOjE2Mjc0NTg3NjUuMjE5MzQzLCJuYmYiOjE2Mjc0NTg3NjUuMjE5MzQ3LCJleHAiOjE2NTg5OTQ3NjUuMjE0ODQ2LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.SuaFzz0IwWRi3_KU_F7UUUVw2lre-kKdE69TDHrRhRK6aCoKgqXMn4uOsbiTuTw7y_eVNlbhTOQ741uEUxsoONhQlWRFf8NLKLjgsVuHbOjyvEJPBXYrPYswY958rZE76f_-VpSs9Vm5fbpPSotjyaoL4dWB_LWgt_xWbr_p4gnBYL1Jw1CJf0iCBQNiiPxBC2zwLHJ2j6jX4-eaYzqCUIRGlW8B1soDPP2s9O4kHrXjEEz-5otbKjrpQu5Sts_rUKULH9dVzEljU0RpiyzFZzZYYJOcVuRzlh4gPxcJ1-aed6dnckoEakP2VxkBPwaRER5DOZZ6qaBixlX3wYMirdgOA2zkJHRSvYklVvgHbFlJohUyd6mEjtjJKlvf5x3BfenB5idM6Z1kGwMB4KlVdhT0_93sHmG_jTMp2gvDkfM9ZvvgbMWGvmxG3KvMYjU79y0iqeu7v6Ag3GQNTxAesDSPUiKmkZ0mf_KjIO4KXbeC7wAff4n1_ZzA2Y9b-KliMY4GgGXyXIGStocQPNumRuE_JNAu5rHiSEDs6PElw6JuQcgH_yissmHgo0-lixz6HcYjzFwPoZLaZ5ScsjZSVefsrTjr--zCx3fihoLy2O0smQ-DaXjPYdr2OpQdMR-Je1Ndk8ei2bVHBZwFM7dHHBeY-hO8qiLq452MgiEu4Dk',
+
+        ])->get($url,[
+            'search' => $request->search
+        ]);
+        dd($response);
+        $users = $response->json()['data'];
+
+        return view ('welcome', compact('users'));
     }
+
+    
 
     public function store(Request $request)
     {
-        $url = 'http://api-training.test/api/users/create';
+        $url = 'http://api-training.test/api/v1/users/create';
         $response = Http::withHeaders([
             'Accept' => 'application/json',
+            'Authorization' => 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYzVlNDg0MjhlMmRmNTYwNjVjNjBjODNmOGM3Y2JhN2I2MGJlODExYWMwZmJkNzQyNjZiNDI0Zjc4M2IyZjgzODVhNWIyMjE4NzM5Zjg4MDciLCJpYXQiOjE2Mjc0NTg3NjUuMjE5MzQzLCJuYmYiOjE2Mjc0NTg3NjUuMjE5MzQ3LCJleHAiOjE2NTg5OTQ3NjUuMjE0ODQ2LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.SuaFzz0IwWRi3_KU_F7UUUVw2lre-kKdE69TDHrRhRK6aCoKgqXMn4uOsbiTuTw7y_eVNlbhTOQ741uEUxsoONhQlWRFf8NLKLjgsVuHbOjyvEJPBXYrPYswY958rZE76f_-VpSs9Vm5fbpPSotjyaoL4dWB_LWgt_xWbr_p4gnBYL1Jw1CJf0iCBQNiiPxBC2zwLHJ2j6jX4-eaYzqCUIRGlW8B1soDPP2s9O4kHrXjEEz-5otbKjrpQu5Sts_rUKULH9dVzEljU0RpiyzFZzZYYJOcVuRzlh4gPxcJ1-aed6dnckoEakP2VxkBPwaRER5DOZZ6qaBixlX3wYMirdgOA2zkJHRSvYklVvgHbFlJohUyd6mEjtjJKlvf5x3BfenB5idM6Z1kGwMB4KlVdhT0_93sHmG_jTMp2gvDkfM9ZvvgbMWGvmxG3KvMYjU79y0iqeu7v6Ag3GQNTxAesDSPUiKmkZ0mf_KjIO4KXbeC7wAff4n1_ZzA2Y9b-KliMY4GgGXyXIGStocQPNumRuE_JNAu5rHiSEDs6PElw6JuQcgH_yissmHgo0-lixz6HcYjzFwPoZLaZ5ScsjZSVefsrTjr--zCx3fihoLy2O0smQ-DaXjPYdr2OpQdMR-Je1Ndk8ei2bVHBZwFM7dHHBeY-hO8qiLq452MgiEu4Dk',
+
         ])->post($url,[
             'name' => $request->name,
             'email' => $request->email,
@@ -30,7 +51,7 @@ class UserController extends Controller
 
     public function show($id)
     {
-        $url = 'http://api-training.test/api/users/show/'.$id;
+        $url = 'http://api-training.test/api/v1/users/show/'.$id;
         $response = Http::get($url);
         // dd($response->object()->data);
         $user = $response->object()->data;
@@ -39,9 +60,11 @@ class UserController extends Controller
 
     public function update(Request $request, $id)
     {
-        $url = 'http://api-training.test/api/users/update/'.$id;
+        $url = 'http://api-training.test/api/v1/users/update/'.$id;
         $response = Http::withHeaders([
-            'Accept' => 'application/json'
+            'Accept' => 'application/json',
+            'Authorization' => 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYzVlNDg0MjhlMmRmNTYwNjVjNjBjODNmOGM3Y2JhN2I2MGJlODExYWMwZmJkNzQyNjZiNDI0Zjc4M2IyZjgzODVhNWIyMjE4NzM5Zjg4MDciLCJpYXQiOjE2Mjc0NTg3NjUuMjE5MzQzLCJuYmYiOjE2Mjc0NTg3NjUuMjE5MzQ3LCJleHAiOjE2NTg5OTQ3NjUuMjE0ODQ2LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.SuaFzz0IwWRi3_KU_F7UUUVw2lre-kKdE69TDHrRhRK6aCoKgqXMn4uOsbiTuTw7y_eVNlbhTOQ741uEUxsoONhQlWRFf8NLKLjgsVuHbOjyvEJPBXYrPYswY958rZE76f_-VpSs9Vm5fbpPSotjyaoL4dWB_LWgt_xWbr_p4gnBYL1Jw1CJf0iCBQNiiPxBC2zwLHJ2j6jX4-eaYzqCUIRGlW8B1soDPP2s9O4kHrXjEEz-5otbKjrpQu5Sts_rUKULH9dVzEljU0RpiyzFZzZYYJOcVuRzlh4gPxcJ1-aed6dnckoEakP2VxkBPwaRER5DOZZ6qaBixlX3wYMirdgOA2zkJHRSvYklVvgHbFlJohUyd6mEjtjJKlvf5x3BfenB5idM6Z1kGwMB4KlVdhT0_93sHmG_jTMp2gvDkfM9ZvvgbMWGvmxG3KvMYjU79y0iqeu7v6Ag3GQNTxAesDSPUiKmkZ0mf_KjIO4KXbeC7wAff4n1_ZzA2Y9b-KliMY4GgGXyXIGStocQPNumRuE_JNAu5rHiSEDs6PElw6JuQcgH_yissmHgo0-lixz6HcYjzFwPoZLaZ5ScsjZSVefsrTjr--zCx3fihoLy2O0smQ-DaXjPYdr2OpQdMR-Je1Ndk8ei2bVHBZwFM7dHHBeY-hO8qiLq452MgiEu4Dk',
+
         ])
         ->post($url, 
         [
@@ -54,9 +77,11 @@ class UserController extends Controller
     
     public function delete($id)
     {
-        $url = 'http://api-training.test/api/users/'.$id;
+        $url = 'http://api-training.test/api/v1/users/'.$id;
         $response = Http::withHeaders([
-            'Accept' => 'application/json'
+            'Accept' => 'application/json',
+            'Authorization' => 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYzVlNDg0MjhlMmRmNTYwNjVjNjBjODNmOGM3Y2JhN2I2MGJlODExYWMwZmJkNzQyNjZiNDI0Zjc4M2IyZjgzODVhNWIyMjE4NzM5Zjg4MDciLCJpYXQiOjE2Mjc0NTg3NjUuMjE5MzQzLCJuYmYiOjE2Mjc0NTg3NjUuMjE5MzQ3LCJleHAiOjE2NTg5OTQ3NjUuMjE0ODQ2LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.SuaFzz0IwWRi3_KU_F7UUUVw2lre-kKdE69TDHrRhRK6aCoKgqXMn4uOsbiTuTw7y_eVNlbhTOQ741uEUxsoONhQlWRFf8NLKLjgsVuHbOjyvEJPBXYrPYswY958rZE76f_-VpSs9Vm5fbpPSotjyaoL4dWB_LWgt_xWbr_p4gnBYL1Jw1CJf0iCBQNiiPxBC2zwLHJ2j6jX4-eaYzqCUIRGlW8B1soDPP2s9O4kHrXjEEz-5otbKjrpQu5Sts_rUKULH9dVzEljU0RpiyzFZzZYYJOcVuRzlh4gPxcJ1-aed6dnckoEakP2VxkBPwaRER5DOZZ6qaBixlX3wYMirdgOA2zkJHRSvYklVvgHbFlJohUyd6mEjtjJKlvf5x3BfenB5idM6Z1kGwMB4KlVdhT0_93sHmG_jTMp2gvDkfM9ZvvgbMWGvmxG3KvMYjU79y0iqeu7v6Ag3GQNTxAesDSPUiKmkZ0mf_KjIO4KXbeC7wAff4n1_ZzA2Y9b-KliMY4GgGXyXIGStocQPNumRuE_JNAu5rHiSEDs6PElw6JuQcgH_yissmHgo0-lixz6HcYjzFwPoZLaZ5ScsjZSVefsrTjr--zCx3fihoLy2O0smQ-DaXjPYdr2OpQdMR-Je1Ndk8ei2bVHBZwFM7dHHBeY-hO8qiLq452MgiEu4Dk',
+
         ])
         ->delete($url);
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,56 @@
-<h1>List User from Api-Dev-Training</h1>
+{{-- <h1>List User from Api-Dev-Training</h1>
 
 @foreach($users as $user)
     <h4>{{ $user['name'] }}</h4>
     <h4>{{ $user['email'] }}</h4>
     <h4>{{ $user['created_at'] }}</h4>
     <br>
-@endforeach
+@endforeach --}}
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+</head>
+<body>
+  
+<table id="customers">
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Created At</th>
+        </tr>
+    @foreach($users as $user)
+        <tr>
+            <td>{{ $user['name'] }}</td>
+            <td>{{ $user['email'] }}</td>
+            <td>{{ $user['created_at'] }}</td>
+        </tr>
+    @endforeach
+</table>
+
+</body>
+</html>
