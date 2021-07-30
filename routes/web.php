bdcb47fa2559e8e8caef496a219faaa899615c9e
Aliff <aliff.rosli96@gmail.com>
2021-07-30 09:16:51 +0800

    Route adjusted and added

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,21 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/users','App\Http\Controllers\API\UserController@index')->name('index');
 Route::get('/users/create','App\Http\Controllers\API\UserController@store')->name('store');
 Route::get('/users/{user}','App\Http\Controllers\API\UserController@show')->name('edit');
 Route::get('/users/show/{user}','App\Http\Controllers\API\UserController@update')->name('update');
 Route::get('/users/delete/{id}','App\Http\Controllers\API\UserController@delete')->name('delete');
+
+Route::get('/inventory','App\Http\Controllers\API\UserController@index')->name('inventory-index');
+Route::get('/inventory/create','App\Http\Controllers\API\UserController@store')->name('inventory-store');
+Route::get('/inventory/{inventories}','App\Http\Controllers\API\UserController@show')->name('inventory-edit');
+Route::get('/inventory/show/{inventories}','App\Http\Controllers\API\UserController@update')->name('inventory-update');
+Route::get('/inventory/delete/{id}','App\Http\Controllers\API\UserController@delete')->name('inventory-delete');
+
 
 
